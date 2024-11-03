e11fa438a7730dc11f90a64db7df28853f749905
arrinal <arrinalsf@gmail.com>
2024-11-03 11:08:32 +0700

    Initial Commit

---
+++ Fraz AI/Fraz_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fraz_AIApp.swift
+//  Fraz AI
+//
+//  Created by Arrinal Sholifadliq on 03/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Fraz_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fraz AITests/Fraz_AITests.swift
@@ -0,0 +1,17 @@
+//
+//  Fraz_AITests.swift
+//  Fraz AITests
+//
+//  Created by Arrinal Sholifadliq on 03/11/24.
+//
+
+import Testing
+@testable import Fraz_AI
+
+struct Fraz_AITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
